3f386d4ebc55f9f38fde96f3731c607744a70cee
mtanaka <matthew.s.tanaka@gmail.com>
2022-03-09 19:44:20 -0800

    dded dotenv package and created schema_aws.sql

--- db/index.js
+++ db/index.js
@@ -1,12 +1,21 @@
 // set up connection to db here
 const { Pool } = require('pg');
 
+require('dotenv').config();
+
+// const pool = new Pool({
+//   user: 'mtanaka',
+//   database: 'qna',
+//   port: 5432,
+// });
+
 const pool = new Pool({
-  user: 'mtanaka',
-  database: 'qna',
-  port: 5432,
-});
-// await pool.connect();
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 5432,
+  user: process.env.USER || 'mtanaka',
+  password: process.end.PASSWORD || '',
+  database: process.env.DATABASE || 'qna',
+})
 
 module.exports = {
   query: (text, params, callback) => pool.query(text, params, callback),
